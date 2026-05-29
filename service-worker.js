d81107c3e846251791b4e46e4ae458ac1beabb7d
poilochio <poilochio@users.noreply.github.com>
2026-05-29 21:48:49 +0000

    deploy: dcfb20d796cdf9faf5de6286a49a994b764687e6

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (event) => {
+  console.log('Service worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activated');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
